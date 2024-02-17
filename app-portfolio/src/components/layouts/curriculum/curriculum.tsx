8f01dc321b91b25069db8da88537075f479eede0
Vinícius Gabriel <viniciusggabriel989@protonmail.com>
2024-02-17 00:26:38 -0300

    Criação do novo componente de curriculum

--- app-portfolio/src/components/layouts/curriculum/curriculum.tsx
+++ app-portfolio/src/components/layouts/curriculum/curriculum.tsx
@@ -1,5 +1,45 @@
+import { Download, Eye, ArrowUpToLine } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 const Curriculum = () => {
-  return <section id="curriculum">sdas</section>;
+  const anchorStyle = "flex justify-between items-center w-7/12";
+
+  return (
+    <section id="#curriculum" className="pb-5">
+      <Popover>
+        <PopoverTrigger asChild className="flex m-auto">
+          <Button
+            variant={"default"}
+            className="flex justify-between items-center gap-5"
+          >
+            <span>Curriculum</span> <ArrowUpToLine size={20} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="flex flex-col items-center mb-2 gap-3">
+          <a
+            href="https://drive.google.com/uc?export=download&id=1Ig990ns4JSUq8DOXak0vPUSJ2yK5T06B"
+            download
+            className={anchorStyle}
+          >
+            <span>Download</span> <Download size={20} />
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1Ig990ns4JSUq8DOXak0vPUSJ2yK5T06B/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={anchorStyle}
+          >
+            <span>Visualizar</span> <Eye size={20} />
+          </a>
+        </PopoverContent>
+      </Popover>
+    </section>
+  );
 };
 
 export default Curriculum;
